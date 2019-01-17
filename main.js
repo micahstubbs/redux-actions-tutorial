3e66e79fad4e8b4f4558b6fb56ee566abb2e144e
micahstubbs <micah.stubbs@gmail.com>
2019-01-17 14:52:38 -0800

    wire up decrement button as well

--- main.js
+++ main.js
@@ -2,27 +2,31 @@ const defaultState = { counter: 0 };
 const content = document.getElementById('content');
 
 const { createStore } = window.Redux;
-const { createAction, handleAction } = window.ReduxActions;
+const { createAction, handleActions } = window.ReduxActions;
 
-// create an action
+// create actions
 const increment = createAction('INCREMENT');
+const decrement = createAction('DECREMENT');
 
 // write a reducer to handle the action
-const reducer = handleAction(
-  increment,
-  (state, action) => ({
-    ...state,
-    counter: state.counter + 1
-  }),
+const reducer = handleActions(
+  {
+    [increment]: state => ({ ...state, counter: state.counter + 1 }),
+    [decrement]: state => ({ ...state, counter: state.counter - 1 })
+  },
   defaultState
 );
 
 // create a store
 const store = createStore(reducer, defaultState);
 
-document.getElementById('increment').addEventListener('click', () => {
-  store.dispatch(increment());
-});
+document
+  .getElementById('increment')
+  .addEventListener('click', () => store.dispatch(increment()));
+
+document
+  .getElementById('decrement')
+  .addEventListener('click', () => store.dispatch(decrement()));
 
 // render the counter value
 const render = () => {
